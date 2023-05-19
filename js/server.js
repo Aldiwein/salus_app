30fff3cbf4e604548e1a2c1ea3889c9451b8c9ec
Aldiwein <megagewinnspiel@gmail.com>
2023-05-19 19:14:40 +0200

    mongoDB login funktioniert

--- js/server.js
+++ js/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb://localhost/salus', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://aldiwein:2DhHAkvVgXPnLiMf@salus.l4oqyyw.mongodb.net/salus', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
